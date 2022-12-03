282898d332153d93cfc61648c6eb17db72360525
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-12-03 14:59:36 +0800

    subias sb

--- zsh/subias/temp.sh
+++ zsh/subias/temp.sh
@@ -7,3 +7,10 @@ function domd() {
 function doast() {
   pandoc -s -t json $1
 }
+
+function hxit() {
+  hx $(find $1 -type f)
+}
+
+# replace curl with xx in file la
+# $ awk '{sub(/curl/,"xx")}1' la

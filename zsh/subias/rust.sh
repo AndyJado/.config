56e0f2e2805e8a0f51a25ecc9707eb500a3bd3f1
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-02-10 14:21:28 +0800

    update rust dev

--- zsh/subias/rust.sh
+++ zsh/subias/rust.sh
@@ -1,12 +1,16 @@
 alias rua='vi ~/.config/zsh/subias/rust.sh'
 alias ha='cd ~/Taste/playgrun/examples/ && jj '
+
+# CARGO:
 alias la='cargo run --example'
 alias cabi='cargo run --bin'
+alias cadow='cargo install --path=.'
 # RUST_DEV=======================================================
 alias gp='git pull upstream master'
 alias pi='./x.py '
-alias ti='cd ~/Zf/rust && ./x.py test src/test/ui/ --pass check'
-alias ta='./x.py test src/test/ui/ --incremental --pass check --keep-stage 1'
+alias ti='./x.py test tests/ui/ --pass check'
+alias ta='./x.py test tests/ui/ --incremental --pass check --keep-stage 1'
+alias sti='mainframer ./x.py test tests/ui/ --incremental --pass check --keep-stage 1'
 alias ki='rm -rf src/tools/cargo && git reset --hard'
 
 function pti() {

bdba12b2dbfcc4c3b91573f1ffa17efac2ff17fb
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-02-27 16:22:10 +0800

    sumi & 2gif

--- zsh/subias/misc.sh
+++ zsh/subias/misc.sh
@@ -1,4 +1,15 @@
+alias sumi='hx ~/.config/zsh/subias/misc.sh'
+
 function 2ascii() {
 	echo hex: $(echo "16i $1 P" | dc);
 	echo deci: $(echo "10i $1 P" | dc);
+}
+
+function 2gif() {
+	for i in $(fd -e $1)
+	do
+		var=${i%.*}
+		# echo $var
+	ffmpeg -i $var.$1 -vf 'scale=-1:1000' -r 15 $var.gif
+	done
 }

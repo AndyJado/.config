2b6bed829f9fdec39e5677b4a01f3f31ae628d36
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-02-27 16:22:03 +0800

    notay && flirto

--- zsh/subias/notake.sh
+++ zsh/subias/notake.sh
@@ -1,4 +1,8 @@
+alias notay="hx ~/.config/zsh/subias/notake.sh"
 alias chi="z chitto && jj"
 alias duh="bash /Users/andyjado/Documents/mshell/date-pend.sh >>"
 alias ma="bash /Users/andyjado/Documents/mshell/boma.sh"
 
+function flirt() {
+  z flirto && touch links/$1.md && pbpaste > links/$1.md && cd -
+}

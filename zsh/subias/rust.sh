cecc0a2f5f282e1f2d3c5af9a598f3393b102c4b
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-04-28 09:51:22 +0800

    repo dev update rua

--- zsh/subias/rust.sh
+++ zsh/subias/rust.sh
@@ -12,16 +12,23 @@ alias spi='mainframer ./x.py '
 alias ti='./x.py test tests/ui/ --pass check'
 alias ta='./x.py test tests/ui/ --incremental --pass check --keep-stage 1'
 alias sti='mainframer ./x.py test tests/ui/ --incremental --pass check --keep-stage 1 --bless'
-alias ki='rm -rf src/tools/cargo && git reset --hard'
+# alias ki='rm -rf src/tools/cargo && git reset --hard'
+
+function ki() {
+	git status -s | awk '{print $2}' | xargs rm -rf
+	git reset --hard
+}
 
 function spti() {
 	mainframer ./x.py test $1 --incremental --pass check --keep-stage 1 2>&1 >/dev/null | rg '^[+|-]\s';
 }
 
+# push to remote <branch name>
 function puremo() {
 	git push --force-with-lease origin HEAD:$1
 }
 
+# debugging dkbb at dir dkb
 function dkb() {
 	git restore source.k
 	cargo build

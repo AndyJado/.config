364eef07e9aa1df6ef74765834d61987c3f77aec
AndyJado <101876416+AndyJado@users.noreply.github.com>
2023-10-05 12:53:59 +0800

    subias M+

--- zsh/subias/web.sh
+++ zsh/subias/web.sh
@@ -14,9 +14,8 @@ function killp() {
 }
 
 # getty objects computer https://ftp.lstc.com/objects/ls-dyna/
+# getty user computer https://ftp.lstc.com/user/ls-tasc/LargeExamples/
 function getty() {
 	wget --user=$1 --password=$2 -r -np -nH --cut-dirs=1 --reject "index.html*" $3
 }
 
-# transfer cheat.txt
-transfer(){ if [ $# -eq 0 ];then echo "No arguments specified.\nUsage:\n  transfer <file|directory>\n  ... | transfer <file_name>">&2;return 1;fi;if tty -s;then file="$1";file_name=$(basename "$file");if [ ! -e "$file" ];then echo "$file: No such file or directory">&2;return 1;fi;if [ -d "$file" ];then file_name="$file_name.zip" ,;(cd "$file"&&zip -r -q - .)|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null,;else cat "$file"|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;else file_name=$1;curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;}
